ece849785e0010492d35c68e6978a1ffb0a17d3b
Huynh Tran <huynhname34@gmail.com>
2026-09-18 17:24:12 +0700

    Update optimized Atlas application 2026-09-18T10:24:12.582Z

--- app/server/ssr/__vite_rsc_assets_manifest.js
+++ app/server/ssr/__vite_rsc_assets_manifest.js
@@ -1,15 +1,15 @@
 export default {
-  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-DioqA5sf.js\")",
+  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-B4IhRYqJ.js\")",
   "clientReferenceDeps": {
     "97760a3bd76a": {
       "js": [
-        "/_next/static/chunks/academy-CxQWB_TK.js",
+        "/_next/static/chunks/academy-C6qQI4Io.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/framework-D_rUT4EX.js",
         "/_next/static/chunks/learning-context-Bv3l8Ppz.js",
-        "/_next/static/chunks/assessment-B_7ocgV4.js",
-        "/_next/static/chunks/answers-DXx4nt1T.js",
+        "/_next/static/chunks/assessment-QG473099.js",
+        "/_next/static/chunks/answers-UFbq4bcB.js",
         "/_next/static/chunks/arrow-right-CXIYx7dF.js",
         "/_next/static/chunks/arrow-up-right-CjaRzw2_.js",
         "/_next/static/chunks/book-open-D-rBKcpr.js",
@@ -18,23 +18,23 @@ export default {
         "/_next/static/chunks/headphones-BAAch9Bc.js",
         "/_next/static/chunks/search-CnFW8QwT.js",
         "/_next/static/chunks/sparkles-DaXm8ovv.js",
-        "/_next/static/chunks/target-CghWm_5N.js",
-        "/_next/static/chunks/triangle-alert-DXqC1Pl2.js"
+        "/_next/static/chunks/target-Bbf8531w.js",
+        "/_next/static/chunks/triangle-alert-7XC3d6CD.js"
       ],
       "css": []
     },
     "25b4469b243f": {
       "js": [
-        "/_next/static/chunks/update-notice-D1rDRWeS.js",
+        "/_next/static/chunks/update-notice-DceJaGtc.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js",
-        "/_next/static/chunks/index-DioqA5sf.js"
+        "/_next/static/chunks/index-B4IhRYqJ.js"
       ],
       "css": []
     },
     "9276801271d6": {
       "js": [
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
@@ -42,7 +42,7 @@ export default {
     },
     "0b874ad30386": {
       "js": [
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
@@ -50,7 +50,7 @@ export default {
     },
     "593f344dc510": {
       "js": [
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
@@ -58,7 +58,7 @@ export default {
     },
     "bad85346fc72": {
       "js": [
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
@@ -66,16 +66,16 @@ export default {
     },
     "15c18cfaeeff": {
       "js": [
-        "/_next/static/chunks/layout-segment-context-C68MVU1R.js",
+        "/_next/static/chunks/layout-segment-context-DVJ_i2I7.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
       "css": []
     },
     "8c0f216c4604": {
       "js": [
-        "/_next/static/chunks/index-DioqA5sf.js",
+        "/_next/static/chunks/index-B4IhRYqJ.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js"
       ],
@@ -83,10 +83,10 @@ export default {
     },
     "89c3cac48cb5": {
       "js": [
-        "/_next/static/chunks/streamed-icons-Dzomu6mq.js",
+        "/_next/static/chunks/streamed-icons-NeRiDggP.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-D_rUT4EX.js",
-        "/_next/static/chunks/index-DioqA5sf.js"
+        "/_next/static/chunks/index-B4IhRYqJ.js"
       ],
       "css": []
     }
